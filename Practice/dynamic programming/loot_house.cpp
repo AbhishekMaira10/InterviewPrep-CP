58ca5501be02599eaf7ad48f6e8ae2ec6d742ec8
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-05 16:41:34 +0530

    loot house dp-2 solution

--- Practice/dynamic programming/loot_house.cpp
+++ Practice/dynamic programming/loot_house.cpp
@@ -16,3 +16,14 @@ int getMaxMoney(int arr[], int n) {
     }
     return max(dp[n - 1], dp[n - 2]);
 }
+
+int getMAxMoney_2(int arr[], int n) {
+    int *dp = new int[n];
+    dp[0] = arr[0];
+    if (n > 1)
+        dp[1] = max(arr[1], dp[0]);
+
+    for (int i = 2; i < n; i++)
+        dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
+    return dp[n - 1];
+}
