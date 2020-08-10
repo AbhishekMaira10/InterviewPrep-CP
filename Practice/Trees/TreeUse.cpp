c5fd174b22c8886fc294f94c6c0249b95b12200f
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-10 13:08:09 +0530

    take input and print recursive

--- Practice/Trees/TreeUse.cpp
+++ Practice/Trees/TreeUse.cpp
@@ -2,12 +2,41 @@
 #include "TreeNode.h"
 using namespace std;
 
-int main() {
-    TreeNode<int>* root = new TreeNode<int>(1);
-    TreeNode<int>* node1 = new TreeNode<int>(2);
-    TreeNode<int>* node2 = new TreeNode<int>(3);
+TreeNode<int>* takeInput() {
+    int rootData;
+    cout << "Enter Data";
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
 
-    root->children.push_back(node1);
-    root->children.push_back(node2);
+    int n;
+    cout<< "Enter number of children of" << rootData << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        TreeNode<int>* child = takeInput();
+        root->children.push_back(child);
+    }
+
+    return root;
+}
+
+void printTree(TreeNode<int>* root) {
+
+    if (root == NULL)
+        return;
+    cout << root->data<< ":";
+    for (int i = 0; i < root->children.size(); i++)
+        cout << root->children[i]->data << ",";
+    cout << endl;
+
+    for (int i = 0; i < root->children.size(); i++) {
+        printTree(root->children[i]);
+    }
+
+}
+
+int main() {
+    TreeNode<int>* root = takeInput();
+    printTree(root);
 
 }
