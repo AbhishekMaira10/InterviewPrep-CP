91dc85f675cef23a1654996f8e2ee462e0a68fb7
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-10 18:28:42 +0530

    height of nodes

--- Practice/Trees/tree_level_wise.cpp
+++ Practice/Trees/tree_level_wise.cpp
@@ -79,6 +79,15 @@ TreeNode<int>* maxDataNode(TreeNode<int>* root) {
     return max;
 }
 
+int height(TreeNode<int>* root) {
+    int ans = 0;
+    for (int i = 0; i < root->children.size(); i++) {
+        int childHeight = height(root->children[i]);
+        if (childHeight > ans)
+            ans = childHeight;
+    }
+    return ans + 1;
+}
 
 
 int main() {
