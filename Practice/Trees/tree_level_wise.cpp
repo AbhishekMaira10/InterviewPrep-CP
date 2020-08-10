17d9e76f640a936ba3ad489db86fdcc45d835020
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-10 18:41:30 +0530

    print nodes at level k

--- Practice/Trees/tree_level_wise.cpp
+++ Practice/Trees/tree_level_wise.cpp
@@ -89,6 +89,14 @@ int height(TreeNode<int>* root) {
     return ans + 1;
 }
 
+void printAtLevelK(TreeNode<int>* root, int k) {
+    if (k == 0) {
+        cout << root->data << " ";
+        return;
+    }
+    for (int i = 0; i < root->children.size(); i++)
+        printAtLevelK(root->children[i], k - 1);
+}
 
 int main() {
     TreeNode<int>* root = takeInput();
