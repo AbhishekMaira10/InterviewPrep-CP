50c9b6561e43969b6608d26c1c3f32157ca4721f
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-10 14:00:51 +0530

    print levelwise

--- Practice/Trees/tree_level_wise.cpp
+++ Practice/Trees/tree_level_wise.cpp
@@ -30,7 +30,30 @@ TreeNode<int>* takeInput() {
     return root;
 }
 
+void printTree(TreeNode<int>* root) {
+    if (root == NULL)
+        return;
+
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data<< ":";
+
+        for (int i = 0; i < front->children.size(); i++) {
+            if (i < ((front->children.size()) - 1))
+                cout << front->children[i]->data << ",";
+            else
+                cout << front->children[i]->data;
+            pendingNodes.push(front->children[i]);
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     TreeNode<int>* root = takeInput();
-
+    printTree(root);
 }
