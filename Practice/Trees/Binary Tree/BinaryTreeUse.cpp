338320e22a36545ae3d6eb3dab3d1b1ed67d5bde
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-17 12:44:30 +0530

    add printBinaryRecursive function

--- Practice/Trees/Binary Tree/BinaryTreeUse.cpp
+++ Practice/Trees/Binary Tree/BinaryTreeUse.cpp
@@ -2,12 +2,37 @@
 #include "BinaryTreeNode.h"
 using namespace std;
 
+BinaryTreeNode<int> *takeInputRecursive() {}
+
+void printBinaryTree(BinaryTreeNode<int> *root)
+{
+  if (root == NULL)
+    return;
+
+  cout << root->data << ":";
+  if (root->left != NULL)
+  {
+    cout << "L" << root->left->data << " ";
+  }
+  if (root->right != NULL)
+  {
+    cout << "R" << root->right->data;
+  }
+  cout << endl;
+  printBinaryTree(root->left);
+  printBinaryTree(root->right);
+}
+
 int main()
 {
+  /*
   BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
   BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
   BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+  */
 
-  root->left = node1;
-  root->right = node2;
+  BinaryTreeNode<int> *root = takeInputRecursive();
+
+  printBinaryTree(root);
+  delete root;
 }
