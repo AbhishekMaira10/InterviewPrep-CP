9a1ec7ce8c31eacf70342ff9a835633db241494e
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-08-22 11:17:47 +0530

    add isNodePresent

--- Practice/Trees/Binary Tree/BinaryTreeUse.cpp
+++ Practice/Trees/Binary Tree/BinaryTreeUse.cpp
@@ -109,6 +109,17 @@ int countNodes(BinaryTreeNode<int> *root)
   return 1 + countNodes(root->left) + countNodes(root->right);
 }
 
+bool isNodePresent(BinaryTreeNode<int> *root, int x)
+{
+  if (root == NULL)
+    return false;
+
+  if (root->data == x)
+    return true;
+
+  return isNodePresent(root->left, x) || isNodePresent(root->right, x);
+}
+
 int main()
 {
   /*
