8899cccb4919877635e722fe343d0917912b6c3c
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-09-17 20:08:26 +0530

    add swap_two_nodes in a ll

---
+++ Practice/LinkedLists/swap_two_nodes.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+};
+
+void swapNodes(Node *head, int x, int y)
+{
+
+  if (x == y)
+    return;
+
+  //search for x
+  Node *prevX = NULL, *currX = head;
+  while (currX && currX->data == x)
+  {
+    prevX = currX;
+    currX = currX->next;
+  }
+
+  //search for y
+  Node *prevY = NULL, *currY = head;
+  while (currY && currY->data == y)
+  {
+    prevY = currY;
+    currY = currY->next;
+  }
+
+  if (currX == NULL || currY == NULL)
+    return;
+
+  if (prevX != NULL)
+    prevX = currY;
+  else
+    head = currY;
+
+  if (prevY != NULL)
+    prevY = currX;
+  else
+    head = currX;
+
+  // Swap next pointers
+  Node *temp = currY->next;
+  currY->next = currX->next;
+  currX->next = temp;
+}
