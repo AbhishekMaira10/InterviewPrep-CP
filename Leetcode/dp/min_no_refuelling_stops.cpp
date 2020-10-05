b396bd661f00da3353e273b873d255f0e4ce6805
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-05 16:26:07 +0530

    add min_no_of_refuelling_stops max-heap solution

---
+++ Leetcode/dp/min_no_refuelling_stops.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Priority Queue Solution - Max-Heap
+ Time Complexity - O(NlogN)
+ Space Complexity - O(N)
+ */
+
+class Solution
+{
+public:
+  int minRefuelStops(int target, int startFuel, vector<vector<int>> &stations)
+  {
+    priority_queue<int> passed_stations;
+    int curr_fuel = startFuel;
+    int curr_pos = 0;
+    int stops = 0;
+    int si = 0;
+
+    while (curr_fuel > 0)
+    {
+      curr_pos += curr_fuel;
+      curr_fuel = 0;
+
+      if (curr_pos >= target)
+        break;
+
+      // no of stations passed
+      while (si < stations.size() && stations[si][0] <= curr_pos)
+      {
+        passed_stations.push(stations[si][1]);
+        ++si;
+      }
+
+      //refuel at the best station so far
+      while (!passed_stations.empty())
+      {
+        curr_fuel += passed_stations.top();
+        passed_stations.pop();
+        ++stops;
+      }
+    }
+    return (curr_pos >= target) ? stops : -1;
+  }
+};
