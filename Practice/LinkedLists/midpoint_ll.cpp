99ea19ed70505348dd1a3724c3406ba2fbaea3aa
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-08 11:57:24 +0530

    add midpoint_ll.cpp

---
+++ Practice/LinkedLists/midpoint_ll.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "Node.h"
+using namespace std;
+
+Node *midPoint(Node *head)
+{
+  if (head == NULL)
+    return head;
+
+  Node *slow = head;
+  Node *fast = head->next;
+
+  while (fast != NULL && fast->next != NULL)
+  {
+    fast = fast->next->next;
+    slow = slow->next;
+  }
+  return slow;
+}
