91e6411da8224f93f542ec692499e838d26e5281
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2022-06-20 15:04:04 +0530

    reverse array

---
+++ Love Babbar placement series/Lecture-20/reverse_array.cpp
@@ -0,0 +1,12 @@
+/* Question -
+ https://www.codingninjas.com/codestudio/problems/reverse-the-array_1262298?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio3
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void reverseArray(vector<int> &arr, int m)
+{
+	reverse(arr.begin(), arr.end());
+}
